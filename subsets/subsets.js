9866be78c1fb3c2d63f1dec59f497d8a98c1ad3a
Daesang You <eotkd4791@gmail.com>
2021-08-05 02:57:06 +0900

    Time: 72 ms (92.45%), Space: 40.8 MB (61.98%) - LeetHub

--- subsets/subsets.js
+++ subsets/subsets.js
@@ -4,18 +4,13 @@
  */
 const subsets = function(nums) {
     const answer = [];
-    const visit = new Array(nums.length).fill(false);
     recur(0, []);
     return answer;
     
     function recur(index, arr) {
         answer.push(arr);
         for(let i=index; i<nums.length; i++) {            
-            if(!visit[i]) {
-                visit[i] = true;
-                recur(i, arr.concat(nums[i]));
-                visit[i] = false;
-            }
+            recur(i + 1, arr.concat(nums[i]));    
         }
     }
 };
