db87322c55c1a8a4cce0f4985682b29202207b35
Daesang You <eotkd4791@gmail.com>
2021-08-05 02:23:35 +0900

    Time: 76 ms (79.49%), Space: 40.7 MB (61.98%) - LeetHub

--- subsets/subsets.js
+++ subsets/subsets.js
@@ -3,19 +3,18 @@
  * @return {number[][]}
  */
 const subsets = function(nums) {
-    const answer = [[]];
-    const set = new Set();
-    recur(0, [], 0);
+    const answer = [];
+    const visit = new Array(nums.length).fill(false);
+    recur(0, []);
     return answer;
     
-    function recur(index, arr, visit) {
-        for(let i=index; i<nums.length; i++) {
-            const check = visit|(1<<i);
-            if(!set.has(check)) {
-                set.add(check);
-                const next = arr.concat(nums[i]);
-                answer.push(next);
-                recur(index + 1, next, check);
+    function recur(index, arr) {
+        answer.push(arr);
+        for(let i=index; i<nums.length; i++) {            
+            if(!visit[i]) {
+                visit[i] = true;
+                recur(i, arr.concat(nums[i]));
+                visit[i] = false;
             }
         }
     }
