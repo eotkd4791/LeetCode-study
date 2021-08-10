6dc8b74c05ee969a57345d78b2f11143b42eba35
Daesang You <eotkd4791@gmail.com>
2021-08-10 23:36:07 +0900

    Time: 248 ms (83.58%), Space: 60.8 MB (62.84%) - LeetHub

--- daily-temperatures/daily-temperatures.js
+++ daily-temperatures/daily-temperatures.js
@@ -2,19 +2,43 @@
  * @param {number[]} temperatures
  * @return {number[]}
  */
+class Stack {
+    constructor() {
+        this._data = [];
+    }
+    push(arg) {
+        this._data.push(arg);
+    }
+    pop() {
+        if(this.empty()) {
+           throw new Error('Nothing To Pop!');
+        }
+        return this._data.pop();
+    }
+    size() {
+        return this._data.length;
+    }
+    top() {
+        return this._data[this.size() - 1];
+    }
+    empty() {
+        return this.size() === 0;
+    }
+}
+
 const dailyTemperatures = function(temperatures) {
-    const stack = [];
+    const stack = new Stack();
     const answer = new Array(temperatures.length).fill(0);
     
     for(let i=0; i<temperatures.length; i++) {
-        while(stack.length > 0 && temperatures[stack[stack.length - 1]] < temperatures[i]) {
+        while(!stack.empty() && temperatures[stack.top()] < temperatures[i]) {
             const index = stack.pop();
             answer[index] = i - index;
         }
         stack.push(i);
     }
-    while(stack.length > 0) {
+    while(!stack.empty()) {
         answer[stack.pop()] = 0;
     }
-    return answer;
+    return answer;  
 };
