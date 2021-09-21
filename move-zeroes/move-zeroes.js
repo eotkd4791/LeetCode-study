9a8e56ac8fce3cc10eb74f42c57a1efd268c6892
Daesang You <eotkd4791@gmail.com>
2021-09-21 20:54:46 +0900

    Time: 84 ms (60.39%), Space: 40 MB (97.03%) - LeetHub

--- move-zeroes/move-zeroes.js
+++ move-zeroes/move-zeroes.js
@@ -3,15 +3,11 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 const moveZeroes = function(nums) {
-    nums.sort((a, b) => {
-        if(a !== 0 && b !== 0) {
-            return 1;
-        } else {
-            if(a === 0) {
-                return 1;
-            } else {
-                return -1;
-            }
-        }
-    });
+    nums.sort((a, b) => 
+        a !== 0 && b !== 0
+            ? 1
+            : a === 0
+            ? 1
+            : -1
+    );
 };
