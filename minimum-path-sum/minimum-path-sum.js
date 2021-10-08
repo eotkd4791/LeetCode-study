761014da5efab2e84645994cccc94116e6b0fcb3
Daesang You <eotkd4791@gmail.com>
2021-10-08 23:14:39 +0900

    Time: 88 ms (50.24%), Space: 41.6 MB (25.91%) - LeetHub

---
+++ minimum-path-sum/minimum-path-sum.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+const minPathSum = function(grid) {
+    const m = grid.length;
+    const n = grid[0].length;
+    const isInside = (x, y) => 0 <= x && x < m && 0 <= y && y < n;
+    
+    const dp = JSON.parse(JSON.stringify(
+        Array(m).fill(
+            Array(n).fill(987654321)
+        )
+    ));
+    return recur(m-1, n-1);
+    
+    function recur(x, y) {
+        if(x === 0 && y === 0) {
+            return grid[x][y];
+        }
+        if(dp[x][y]<987654321) {
+            return dp[x][y];
+        }
+        if(isInside(x-1, y)) {
+            const top = Math.min(recur(x-1, y), dp[x-1][y]) + grid[x][y];
+            dp[x][y] = Math.min(dp[x][y], top);
+        }
+        if(isInside(x, y-1)) {
+            const left = Math.min(recur(x, y-1), dp[x][y-1]) + grid[x][y];
+            dp[x][y] = Math.min(dp[x][y], left);
+        }
+        return dp[x][y];
+    }
+};           
