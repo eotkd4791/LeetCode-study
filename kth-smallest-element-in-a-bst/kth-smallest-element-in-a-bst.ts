e2ff22b6aff7c6505549c48eaf33643c3e5dc997
Daesang You <eotkd4791@gmail.com>
2021-10-23 12:49:46 +0900

    Time: 123 ms (38.54%), Space: 45.7 MB (79.17%) - LeetHub

---
+++ kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.ts
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+type Tree = TreeNode | null;
+
+function kthSmallest(root: Tree, k: number): number {
+   const values: number[] = [];
+    recur(root);
+    return values[k-1];
+    
+    
+    function recur(tree: Tree) {
+        tree.left && recur(tree.left);
+        values.push(tree.val);
+        tree.right && recur(tree.right);
+    }
+};
