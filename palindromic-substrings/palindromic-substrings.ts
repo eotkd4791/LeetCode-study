0583cea93e17c3f376ee9214a259a8eebdff74b9
Daesang You <eotkd4791@gmail.com>
2021-10-23 13:29:45 +0900

    Time: 383 ms (20.00%), Space: 81.5 MB (6.67%) - LeetHub

---
+++ palindromic-substrings/palindromic-substrings.ts
@@ -0,0 +1,32 @@
+function countSubstrings(s: string): number {
+    const n = s.length;
+    let result: number = 0;
+    
+    const dp = JSON.parse(JSON.stringify(
+        Array(n).fill(
+            Array(n).fill(false)
+        )
+    ));
+
+    for(let i=0; i<n; i++) {
+        dp[i][i] = true;
+    }
+
+    for(let i=1; i<n; i++) {
+        for(let j=0; j<i; j++) {
+            if(i-j < 3) {
+                dp[j][i] = s[i] === s[j];
+            }
+            else {
+                dp[j][i] = dp[j+1][i-1] && s[i] === s[j];
+            }
+        }
+    }
+
+    for(let i=0; i<n; i++) {
+        for(let j=0; j<n; j++) {
+            dp[i][j] && result++;
+        }
+    }
+    return result;
+};
