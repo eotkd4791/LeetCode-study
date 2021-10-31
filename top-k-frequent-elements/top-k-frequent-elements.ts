d9c1642c6ef20ee9998f89f7fdd8a04b00b4c2f8
Daesang You <eotkd4791@gmail.com>
2021-11-01 00:03:22 +0900

    Time: 92 ms (90.16%), Space: 42.5 MB (60.66%) - LeetHub

--- top-k-frequent-elements/top-k-frequent-elements.ts
+++ top-k-frequent-elements/top-k-frequent-elements.ts
@@ -1,53 +1,27 @@
 function topKFrequent(nums: number[], k: number): number[] {
-    const numberCount = getNumberCount();
-    const frequency = getFrequency();
-    return getTopKFrequent();
+  const numsToFrequencyMap: { [key: number]: number } = {};
     
-    function getTopKFrequent() {
-        let result = [];
-        for(let i=frequency.length -1 ; i>= 0; i--) {
-            if(isCompleteSelecting()) {
-                return result;
-            }
-            frequency[i] && (result = extractFrequency(i, result));
-        }
-        return result;
-        
-        function extractFrequency(i, listKFrequency) {
-            for(let j=0; j<frequency[i].length; j++) {
-                k--;
-                listKFrequency.push(frequency[i][j]);
-                if(isCompleteSelecting()) {
-                    break;
-                }
-            }
-            return listKFrequency;
-        }
-        
-        function isCompleteSelecting() {
-            return k === 0;
-        }
-    }
-    
-    function getFrequency() {
-        const result = [[]];
-        for(const num in numberCount) {
-            const key = numberCount[num];
-            result.hasOwnProperty(key)
-                ? result[key].push(num)
-                : result[key] = [num];
-        }    
-        return result;
-    }
-    
-    function getNumberCount() {
-        const result = {};
-        for(let i=0; i<nums.length; i++) {
-            const key = nums[i];
-            result.hasOwnProperty(key)
-                ? result[key]++
-                : result[key] = 1;
-        }
-        return result;
-    }
-};
+  for (let i = 0; i < nums.length; i++) {
+    const element = nums[i];
+      
+    numsToFrequencyMap.hasOwnProperty(element)
+        ? numsToFrequencyMap[element] ++
+        : numsToFrequencyMap[element] = 1;
+  }
+
+  const order: [number, number][] = [];
+  for (const key in numsToFrequencyMap) {
+    const frequency = numsToFrequencyMap[key];
+    order.push([Number(key), Number(frequency)]);
+  }
+
+  order.sort((a, b) => b[1] - a[1]);
+
+  const result: number[] = [];
+  for (let i = 0; i < k; i++) {
+    const element = order[i];
+    const num = element[0];
+    result.push(num);
+  }
+  return result;
+}
