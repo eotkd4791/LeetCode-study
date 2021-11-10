34a5938c8bbdfda144e6d95b09cfc65c21b3c48f
Daesang You <eotkd4791@gmail.com>
2021-11-11 01:01:32 +0900

    Time: 84 ms (87.73%), Space: 47.9 MB (61.25%) - LeetHub

---
+++ container-with-most-water/container-with-most-water.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+const maxArea = function(height) {
+    const n = height.length;
+    let result = 0;
+    
+    let left = 0;
+    let right = n - 1;
+    
+    while(left < right) {
+        result = Math.max(result, Math.min(height[left], height[right]) * (right - left));
+        if(height[left] >= height[right]) {
+            right--;
+        } else {
+            left++;
+        }
+    }
+    return result;
+};
