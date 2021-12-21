9f445ae8b8f0a81f1b05adbd02bb13e836e88be0
Daesang You <eotkd4791@gmail.com>
2021-12-21 16:31:04 +0900

    Time: 84 ms (89.47%), Space: 42.3 MB (13.16%) - LeetHub

---
+++ flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.ts
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+/**
+ Do not return anything, modify root in-place instead.
+ */
+function flatten(root: TreeNode | null): void {
+    const nodes: TreeNode[] = [];
+    recur(root);
+    makeList();
+    
+    function recur(node: TreeNode | null) {
+        if(node === null) {
+            return;
+        }
+        nodes.push(node);
+        recur(node.left);
+        recur(node.right);
+    }
+
+    function makeList() {
+        if(nodes.length < 2) {
+            return;
+        }
+        
+        let curNode = root;
+        
+        if(root) {
+            nodes.forEach(node => {
+                curNode!.left = null;
+                curNode!.right = node;
+                curNode = curNode!.right;
+            });
+        }
+    }
+};
