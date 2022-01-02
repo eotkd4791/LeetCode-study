bae8597af11bea293e23d749d717e3385b4ff75a
Daesang You <eotkd4791@gmail.com>
2022-01-02 22:26:15 +0900

    Time: 96 ms (64.99%), Space: 48.5 MB (84.24%) - LeetHub

---
+++ maximum-subarray/maximum-subarray.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const maxSubArray = function(nums) {
+    let max = -987654321;
+    let sum = 0;
+    
+    nums.forEach((num) => {
+        if(sum < 0) {
+            sum = 0;
+        }
+        
+        sum += num;
+        
+        max = Math.max(max, sum);
+    });
+    
+    return max;
+};
