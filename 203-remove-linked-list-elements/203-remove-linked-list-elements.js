37d351caf751f62b8668a733939a5c7e8910a759
Daesang You <eotkd4791@gmail.com>
2022-03-20 00:58:09 +0900

    Time: 80 ms (91.23%), Space: 46.9 MB (36.83%) - LeetHub

---
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+const removeElements = function(head, val) {
+    const start = new ListNode(0, head);
+    let prev = start;
+    let cur = head;
+    
+    while(cur !== null) {
+        if(cur.val === val) {
+            prev.next = cur.next;
+            cur = cur.next;
+        } else {
+            prev = cur;
+            cur = cur.next;
+        }
+    }
+    
+    return start.next;
+};
